ee2817013375a73fd0e4de198ddd2f8a00f45d98
Haris Nazir <haris.nazir1963@gmail.com>
2021-07-05 17:37:28 +0100

    Fix Views

--- main/views.py
+++ main/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
 from .forms import GetCountry
+import requests
 
 # Create your views here.
 def index(request):
-    form = GetCountry()
+    form = GetCountry(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            iso_code = form.cleaned_data['iso3']
+            return AddToModel(iso_code)
     return render(request, 'country_form.html', {'form': form})
+
+def AddToModel(iso_code):
+    res = requests.get(f"https://restcountries.eu/rest/v2/alpha/{iso_code}")
