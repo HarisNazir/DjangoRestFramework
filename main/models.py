cf1b4a1fd41bfa90633e5bc282597c9add29988c
Haris Nazir <haris.nazir1963@gmail.com>
2021-07-05 16:38:48 +0100

    Added Models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class excel_generation_request(models.Model):
+    id = models.AutoField(primary_key=True)
+    country = models.CharField()
+    status = models.CharField()
+    generated_file = models.CharField()
