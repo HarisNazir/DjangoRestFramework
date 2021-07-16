b2e6a715e5af41e5cd5a3bd28c0b84220fe6aaf7
Haris Nazir <haris.nazir1963@gmail.com>
2021-07-16 16:16:36 +0100

    Added Second Test, issue with response

--- main/tests/test_GenerateExcelPost.py
+++ main/tests/test_GenerateExcelPost.py
@@ -1,3 +1,4 @@
+from django.http import response
 from main.views import GenerateExcel
 import pytest
 from main.models import excel_generation_request
@@ -10,10 +11,10 @@ from rest_framework.response import Response
 
 @pytest.mark.django_db
 def test_GenerateExcel(client):
-    client.post('/api/generate-excel?country="FRA"')
+    client.post('/api/generate-excel?country=FRA')
 
     assert excel_generation_request.objects.count() == 1
     obj = excel_generation_request.objects.all()[0]
     assert obj.country == "FRA"
 
-    assert Response("/api/excel-generation-request/1") == f"/api/excel-generation-request/{obj.id}"
+    assert response.url == f"/api/excel-generation-request/{obj.id}"
