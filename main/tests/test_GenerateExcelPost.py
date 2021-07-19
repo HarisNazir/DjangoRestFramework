8f7667cef041dd2a538b5569e6c4ea0f6e2e33b4
Haris Nazir <haris.nazir1963@gmail.com>
2021-07-19 13:44:24 +0100

    Generate Excel Test

--- main/tests/test_GenerateExcelPost.py
+++ main/tests/test_GenerateExcelPost.py
@@ -4,17 +4,11 @@ import pytest
 from main.models import excel_generation_request
 from rest_framework.response import Response
 
-# ResultURL = GenerateExcel.data
-
-# def CheckResultURLTest(ResultURL):
-#     assert ResultURL == "/api/excel-generation-request/1", "Incorrect URL"
-
 @pytest.mark.django_db
 def test_GenerateExcel(client):
-    client.post('/api/generate-excel?country=FRA')
+    response = client.post('/api/generate-excel?country=FRA')
 
     assert excel_generation_request.objects.count() == 1
     obj = excel_generation_request.objects.all()[0]
     assert obj.country == "FRA"
-
-    assert response.url == f"/api/excel-generation-request/{obj.id}"
+    assert response.json()["result-url"] == f"/api/generate-excel/{obj.id}"
---
+++ main/tests/test_StatusDoneTest.py
@@ -0,0 +1,5 @@
+# import pytest
+
+# @python.mark.django
+# def test_StatusDone(client):
+    
