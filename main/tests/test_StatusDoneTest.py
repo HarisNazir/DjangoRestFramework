021ed1fe604c8b7f5d47cc5ab91c8707a6152784
Haris Nazir <haris.nazir1963@gmail.com>
2021-07-20 14:02:26 +0100

    Need to make test compatible with Celery

--- main/tests/test_StatusDoneTest.py
+++ main/tests/test_StatusDoneTest.py
@@ -1,5 +1,11 @@
-# import pytest
+from main.models import excel_generation_request
+import pytest
 
-# @python.mark.django
-# def test_StatusDone(client):
-    
+@pytest.mark.django_db
+@pytest.mark.celery(result='redis://')
+def test_StatusDone(client, celery_worker):
+    client.post('api/generate-excel?country=FRA')
+
+    assert excel_generation_request.objects.count() == 1
+    obj = excel_generation_request.objects.all()[0]
+    assert obj.status != 'pending'
